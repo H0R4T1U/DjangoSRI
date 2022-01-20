eb835c24524bee9c917e88340ecc2d31368b8a2e
H0R4T1U <horatiubanciu11@gmail.com>
2022-01-20 20:05:54 +0200

    headless=True

--- Utils/Scrapper.py
+++ Utils/Scrapper.py
@@ -1,28 +1,31 @@
-from django.conf import settings as django_settings
-import os
-
-
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 import pandas as pd
 
 
 def scrap(website):
     df = pd.DataFrame()
+    options = Options()
+    options.headless = True
+
+    driver = webdriver.Firefox(options=options)
 
 
-    driver = webdriver.Firefox()
-
-    driver.get('https://www.pcgarage.ro/notebook-laptop/')
+    driver.get(website.url)
     data = {}
 
     content = driver.page_source
     soup = BeautifulSoup(content, features="html.parser")
     for div in soup.find_all('div', class_=website.container_class):
         for ScrapedClass in website.classes:
-            data[f"{ScrapedClass}"] = div.find('div', class_=ScrapedClass).text
+            try:
+                data[f"{ScrapedClass}"] = div.find('div', class_=ScrapedClass).text
+            except:
+                data[f"{ScrapedClass}"] = "null"
             df =df.append(data,ignore_index=True)
 
 
 
     df.to_csv(f"static/dataframes/{website.name}.csv", index=False, encoding='utf-8')
+    driver.quit()
